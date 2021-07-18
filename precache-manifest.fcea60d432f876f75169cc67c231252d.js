c849fe1e45ecb76c89b83288e6bd81d90778783e
Kuldeep Joshi <38746631+cooldeep5897@users.noreply.github.com>
2021-07-18 11:28:31 +0530

    Updates

---
+++ precache-manifest.fcea60d432f876f75169cc67c231252d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b251fc8842776cf71bc57627000d07",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "e597283f1b328fe090ba",
+    "url": "/my-resume/static/css/2.c0871d5a.chunk.css"
+  },
+  {
+    "revision": "b8363f7b5e00a9d68ee2",
+    "url": "/my-resume/static/css/main.12127ea2.chunk.css"
+  },
+  {
+    "revision": "e597283f1b328fe090ba",
+    "url": "/my-resume/static/js/2.b2bec165.chunk.js"
+  },
+  {
+    "revision": "603f637d7d58e4667a575522771b0bbb",
+    "url": "/my-resume/static/js/2.b2bec165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8363f7b5e00a9d68ee2",
+    "url": "/my-resume/static/js/main.d8c9aa99.chunk.js"
+  },
+  {
+    "revision": "a6efcd828d3624d74f01",
+    "url": "/my-resume/static/js/runtime-main.fb0541e1.js"
+  },
+  {
+    "revision": "d56d3e6db9ade22884a69fe93eb4b455",
+    "url": "/my-resume/static/media/codechef.d56d3e6d.png"
+  },
+  {
+    "revision": "3018b587971c542f9e7b463af3d0e439",
+    "url": "/my-resume/static/media/dark_wall.3018b587.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-resume/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-resume/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-resume/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-resume/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-resume/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e4eef3e4303ee803dbc44ab60e22f148",
+    "url": "/my-resume/static/media/github.e4eef3e4.png"
+  },
+  {
+    "revision": "a9510e92f330064ce16a1dac0b7611bc",
+    "url": "/my-resume/static/media/kuldeep2.a9510e92.jpg"
+  },
+  {
+    "revision": "8a34fa75300b7f9f87b4d0ba78dbbdc1",
+    "url": "/my-resume/static/media/leetcode.8a34fa75.PNG"
+  },
+  {
+    "revision": "33a32219df8fc8e22b1b853d9ac9caf8",
+    "url": "/my-resume/static/media/undraw_static_assets_rpm6.33a32219.svg"
+  }
+]);
